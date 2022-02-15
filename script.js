490e4ebae9584625ad017f8597c587815e193d24
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-02-15 12:04:24 +0100

    Make the book class

--- script.js
+++ script.js
@@ -1,68 +1,48 @@
-let books = [];
 const listSection = document.querySelector('.book-list');
 const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const theForm = document.querySelector('form');
 
-function removeBook(bookitem, i) {
-  const bookBlock = document.getElementById(i);
-  books = books.filter((item) => item !== bookitem);
-  localStorage.setItem('bookInfo', JSON.stringify(books));
-  listSection.removeChild(bookBlock);
-}
-
-function addBookitem(bookitem, i) {
-  const bookBlock = document.createElement('div');
-  bookBlock.classList.add('bookBlock');
-  bookBlock.id = i;
-
-  const removeBtn = document.createElement('button');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.innerText = 'Remove';
-
-  const underLine = document.createElement('hr');
-
-  bookBlock.innerHTML = `<p class="book-title">${bookitem.title}</p>
-    <p class="book-Author">${bookitem.author}</p> `;
-  bookBlock.appendChild(removeBtn);
-  bookBlock.appendChild(underLine);
-  listSection.appendChild(bookBlock);
-
-  removeBtn.onclick = () => {
-    removeBook(bookitem, i);
-  };
-}
-
-function addBook(item) {
-  books.push({
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  });
-
-  localStorage.setItem('bookInfo', JSON.stringify(books));
-  bookTitle.value = '';
-  bookAuthor.value = '';
-  addBookitem(item, (books.length - 1));
-}
-
-function updateUI() {
-  if (localStorage.getItem('bookInfo')) {
-    books = JSON.parse(localStorage.getItem('bookInfo'));
-    books.forEach((bookitem, i) => {
-      addBookitem(bookitem, i);
-    });
-  } else {
-    localStorage.setItem('bookInfo', '');
-    books = [];
+class Books {
+  constructor () {
+    this.title = title;
+    this.author = author;
   }
+
+  save() {
+    title = JSON.parse(bookTitle.value);
+    author = JSON.parse(bookAuthor.value);
+  }
+
+  store() {
+    let boolList = [];
+    boolList.push([title,author]);
+  }
+
+  show () {
+    let titleShow = JSON.stringify(title);
+    let authorShow = JSON.stringify(author);
+
+    const bookShow = document.createElement('ul');
+    bookShow.innerHTML = `
+      <li class="bookRow">
+        <p>"${titleShow}" by ${authorShow}</p>
+        <button id="remove">Remove</button>
+      </li>
+    `
+  }
+
+  remove () {
+    boolList = boolList.filter(item => )
+
+  }
+
 }
 
-updateUI();
+let book = new Books;
 
-theForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addBook({
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  });
-});
+// Add book //
+// event listner =>  click on add : book.store; book.show
+
+// Remove a book //
+// event listner =>  click on remove : book.remove
