fcfcd562411175b1c36ce1a36a8d5f187280ef79
Nicholas Mutale <nicomutale@gmail.com>
2022-02-16 09:16:22 +0200

    add display ui

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ class Book {
     window.location.reload();
   }
 }
+
+static displayUI () {
+  let bookDIV = '';
+  Book.books = JSON.parse(localStorage.getItem('bookInfo'));
+  if(Book.books === null){
+    Book.books = [];
+  }
+  Book.books.forEach((item, index) => {
+    bookDIV += `<div class="bookDIV">
+    <p class='button-info'>"${item.title}" by "${item.author}"</p>
+    <button type="button" class="remove-btn" id="${index}">Remove</button>
+    </div>`;
+  });
+  bookList.innerHTML = bookDIV;
+}
+}
+theForm.addEventListener('submit', () => {
+const newBook = new Book(bookTitle.value, bookAuthor.value);
+newBook.addBook();
+});
+Book.displayUI();
+let removeButton = document.querySelectorAll('.remove-btn');
+removeButton.forEach((item) => item.addEventListener('click',function(){
+let deleteBook = new Book (bookTitle.value, bookAuthor.value, this.id);
+deleteBook.removeBook();
+}));
