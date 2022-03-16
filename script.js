add1596a8d56c6fd69785b315917a3f6bf34ee44
Said Rasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2022-03-16 13:43:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -76,6 +76,25 @@ class UpdateDisplay {
   }
 }
 
+const timeBox = document.querySelector('#date');
+
+function time() {
+  const date = new Date();
+  const locale = navigator.language;
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: 'false',
+  };
+  timeBox.textContent = `${date.toLocaleTimeString(locale, options)}`;
+}
+
+setInterval(time, 1000);
+
 UpdateDisplay.listBtn.forEach((btn, i) => {
   btn.onclick = () => {
     UpdateDisplay.listBtn.forEach((oldBtn) => {
